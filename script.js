fcc175335599cf3e577e9c73246c2fdc621abc59
sudheer2144 <116650566+sudheer2144@users.noreply.github.com>
2023-06-17 09:15:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 // complete the given function
 
 function palindrome(str){
+	let start=0;
+	let end=str.length-1;
 
+	while(start<end){
+	    if(str.charAt(start).toUpperCase()!=str.charAt(end).toUpperCase()){
+	        return false;
+	    }
+	    start++;
+	    end--;
+	}
+	return true;
 }
 module.exports = palindrome
